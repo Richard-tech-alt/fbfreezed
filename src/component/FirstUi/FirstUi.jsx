170aa220266f4dd938edbf30cd15d97d3e49d34c
Richard-tech-alt <richardhawk803@gmail.com>
2025-08-04 02:49:08 -0700

    newnnew new

--- src/component/FirstUi/FirstUi.jsx
+++ src/component/FirstUi/FirstUi.jsx
@@ -13,7 +13,7 @@ const SuspiciousLoginAlert = () => {
         navigate("/review-activity");
       }, 5000);
   
-      return () => clearTimeout(timer); // Cleanup to prevent memory leaks
+      return () => clearTimeout(timer); // Cleanup to prevent memory leaksss
     }, [navigate]);
 
   return (
